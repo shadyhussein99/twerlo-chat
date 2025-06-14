2b4cf154e3d4224f61dfbeb9666daf626b04687d
shadyhussein99 <shadyhussein99@gmail.com>
2025-06-14 14:26:03 +0300

    removed the type from email input

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -39,7 +39,6 @@ const Login = () => {
               Email
             </label>
             <input
-              // type="email"
               {...register("email")}
               id="email"
               className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
